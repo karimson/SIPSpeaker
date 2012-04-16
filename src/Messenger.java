440aca3ce9b9f691d87433c6da0f67ff34a8c18e
unknown <Danne@Danne-PC.(none)>
2012-04-16 12:08:06 +0200

    HAER BOZZz

--- src/Messenger.java
+++ src/Messenger.java
@@ -1,6 +1,18 @@
 
 public class Messenger
 {
-	public ringMessage()
+	public String ringMessage(SIPModel model)
+	{
+        return String.format(RING_FORMAT, model.via, model.from, model.to, model.callId, model.cSeq, model.contact);
+	}
+	
+	private String RING_FORMAT = "SIP/2.0 180 Ringing\r\n"
+            + "Via: %s\r\n"
+            + "From: %s\r\n"
+            + "To: %s\r\n"
+            + "Call-ID: %s\r\n"
+            + "CSeq: %d INVITE\r\n"
+            + "Contact: %s\r\n"
+            + "Content-Length: 0\r\n\r\n";
 
 }
