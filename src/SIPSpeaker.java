063cc7688a32e36cc025910a4e8dd159312a925f
Alen Stukan <stukan@kth.se>
2012-04-20 13:20:41 +0200

    senaste

--- src/SIPSpeaker.java
+++ src/SIPSpeaker.java
@@ -1,19 +1,48 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Properties;
 
 
 public class SIPSpeaker 
 {
-	private static int port;
+	private static int sipPort;
+	private static int httpPort;
+	
 	public static void main(String [] args) throws SocketException, InterruptedException, Exception
 	{
+		Properties prop = new Properties();
+		InputStream is = new FileInputStream("sipspeaker.cfg");
 
+	    prop.load(is);
+	    
+	    prop.getProperty("sip_port", "5070");
+	    
+		
+		for(int i=0; i<args.length; i++)
+		{
+			if(args[i].contains("-c"))
+			{
+				is = new FileInputStream(args[i]);
+				prop.load(is);
+			}
+			if(args[i].contains("-user"))
+			{
+				
+			}
+			if(args[i].contains("-http"))
+			{
+
+			}
+		}
+		
   		WebServer web = new WebServer();
-		web.startWebServer(8000);
+		web.startWebServer(httpPort);
 		
 		SipServer sip = new SipServer();
-		sip.startSipServer(5070);
+		sip.startSipServer(sipPort);
 	}
 	
 	public static String getLocalIP()
@@ -28,15 +57,6 @@ public class SIPSpeaker
 		{
 		}
 		return addr.getHostAddress();
-	}
-	
-	public static void setPort(int port)
-	{
-		SIPSpeaker.port = port;
-	}
-	public static int getPort()
-	{
-		return port;
 	}
 }
 
